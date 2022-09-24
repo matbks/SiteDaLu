62d197a8f6686cf98816c3cc40fd52f46d74723c
Backhaus <44726485+matbks@users.noreply.github.com>
2022-09-24 14:24:28 -0300

    ss

---
+++ src/componentes/NavBar.tsx
@@ -0,0 +1,35 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Card from './Card'
+
+function BasicExample() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container> 
+        <Card/>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto"> 
+            <Nav.Link
+            //  style={{ position: 'relative', fontSize:25}}
+             href="#link">Comprar</Nav.Link>
+            <NavDropdown
+            //  style={{ position: 'relative', left: 20, fontSize:25}} 
+             title="Cadastro" id="basic-nav-dropdown">
+              <NavDropdown.Item
+            //   style={{ position: 'relative', fontSize:20}} 
+              href="cadastro">Cadastro</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item
+              href="login">Login</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
---
+++ src/paginas/Page.tsx
@@ -0,0 +1,27 @@
+import NavBar from "../componentes/NavBar"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from "../componentes/Footer"
+import './style.css' 
+
+function Page() {   // Import result is the URL of your image  
+    return (
+        <div>
+
+            <NavBar />
+
+            <div style={{ backgroundImage: "url(/pudim.png)", 
+                          backgroundRepeat:'no-repeat',
+                          WebkitBackgroundSize:'auto', 
+                          backgroundPositionX:'50%',               
+                          backgroundPositionY:'25%', 
+                          position:'fixed', 
+                          height:'100%', 
+                          width:'100%'}}/>      
+
+            <Footer  />
+
+        </div>
+    );
+}
+
+export default Page;
