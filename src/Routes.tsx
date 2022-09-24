84f75bd8b583c533948c3dca3dc9efde2d144291
Backhaus <44726485+matbks@users.noreply.github.com>
2022-09-24 14:24:25 -0300

    cc

---
+++ src/Routes.tsx
@@ -0,0 +1,16 @@
+import  Page  from "./paginas/Page" 
+import Login from "./paginas/Login"
+import { BrowserRouter as Router,
+         Routes,
+         Route } from "react-router-dom";
+
+export function AppRoutes() {
+    return (
+        <Router> 
+           <Routes>
+            <Route path="/" element={<Page />} />
+            <Route path="/login" element={<Login />} />
+           </Routes>
+        </Router>
+    )
+}
