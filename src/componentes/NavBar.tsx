3eefbf76522a377b806a149deb49288d381adee0
Backhaus <44726485+matbks@users.noreply.github.com>
2022-09-25 06:08:35 -0300

    ss

--- src/componentes/NavBar.tsx
+++ src/componentes/NavBar.tsx
@@ -1,40 +1,21 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Card from './Card'
-import React from 'react'
+import React from 'react';
+import NavBarDesktop from "../componentes/NavBarDesktop"
+import NavBarMobile from "../componentes/NavBarMobile"
 
+function NavBar() {
+  const [width, setWidth] = React.useState(window.innerWidth); 
 
+  if (width < 450) {
+    return <NavBarMobile />;
+  }
 
-function BasicExample() {
-  const [width, setWidth] = React.useState(window.innerWidth);
-  let wid:number = width;
-  return (
-    <Navbar bg="light" expand="lg"  fixed="top">
-      <Container > 
-        <Card width={ width }/>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" >
-          <Nav className="me-auto" style={{ marginLeft:10}}> 
-            <Nav.Link
-            //  style={{ position: 'relative', fontSize:25}}
-             href="Comprar"><h4>Comprar</h4></Nav.Link>
-            <NavDropdown
-            //  style={{ position: 'relative', left: 20, fontSize:25}} 
-             title="Cadastro" id="basic-nav-dropdown">
-              <NavDropdown.Item
-            //   style={{ position: 'relative', fontSize:20}} 
-              href="cadastro"><h4>Cadastro</h4></NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item
-              href="login"><h4>Login</h4></NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+  else if (width < 650) {
+    return <NavBarMobile />;
+  }
+
+  else {
+    return <NavBarDesktop />;
+  }
 }
 
-export default BasicExample;
+export default NavBar;
---
+++ src/componentes/NavBarDesktop.tsx
@@ -0,0 +1,39 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Card from './Card'
+import React from 'react'
+
+
+function BasicExample() {
+  const [width, setWidth] = React.useState(window.innerWidth);
+  let wid:number = width;
+  return (
+    <Navbar bg="light" expand="lg"  fixed="top">
+      <Container > 
+        <Card width={ width }/>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" style={{   }}>
+          <Nav className="me-auto" style={{ marginLeft:10}}> 
+            <Nav.Link
+            //  style={{ position: 'relative', fontSize:25}}
+             href="Comprar" style={{ fontSize:24, position:'relative', left:60}}> Comprar </Nav.Link>
+            <NavDropdown
+            //  style={{ position: 'relative', left: 20, fontSize:25}} 
+             title="Cadastro" id="basic-nav-dropdown" style={{ fontSize:24, position:'relative', left:90    }}>
+              <NavDropdown.Item
+            //   style={{ position: 'relative', fontSize:20}} 
+              href="cadastro"><h6>Cadastro</h6></NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item
+              href="login"><h6>Login</h6></NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
