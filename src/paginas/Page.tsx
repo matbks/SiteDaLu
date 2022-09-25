3c10d54acf3e05871362e8b7d78cc8f711b40847
gaalawliet <89482043+gaalawliet@users.noreply.github.com>
2022-09-24 22:25:07 -0300

    atualização video mp4

--- src/paginas/Page.tsx
+++ src/paginas/Page.tsx
@@ -2,23 +2,37 @@ import NavBar from "../componentes/NavBar"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from "../componentes/Footer"
 import './style.css'
-import Video from "../paginas/img/videopudim.mp4"
-
+import Video from "./videopudim.mp4"
 function Page() {   // Import result is the URL of your image  
     return (
         <div>
 
             <NavBar />
 
-            <div style={{ 
+            <div> 
+            <video autoPlay loop muted 
+            style={{
+                position: "fixed",
+                // height: "100%",
+                // width: "90%",
+                // left: "0%",
+                // top: "12%",
+                objectFit: "cover",
+                // transform: "translate (-50, -50)"
+            }} >
+            
+                <source src={Video} type="video/mp4"/>
+                </video>
+                         {/* style={{ 
                         //   backgroundImage: "url(/pudim.png)", 
                         //   backgroundRepeat:'no-repeat',
-                          WebkitBackgroundSize:'auto', 
-                          backgroundPositionX:'50%',               
-                          backgroundPositionY:'25%', 
-                          position:'fixed', 
-                          height:'100%', 
-                          width:'100%'}}/>      
+                        //   WebkitBackgroundSize:'auto', 
+                        //   backgroundPositionX:'50%',               
+                        //   backgroundPositionY:'25%', 
+                        //   position:'fixed', 
+                        //   height:'100%', 
+                        //   width:'100%'}} */}
+                 </div>      
 
             <Footer  />
 
