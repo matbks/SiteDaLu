2500949cfee433dc93306c4fb508a1e94a655281
Backhaus <44726485+matbks@users.noreply.github.com>
2022-09-25 06:46:39 -0300

    ss

--- src/paginas/Login.tsx
+++ src/paginas/Login.tsx
@@ -76,13 +76,13 @@ function App() {
   );
 
   return (
-    <div>
+    <div style={{position:'relative'}}>
 
     
 
-    <div className="app">   
+    <div className="app" >   
     
-      <div className="login-form">
+      <div className="login-form"  >
       <img src='./pudim.jpg'  style={{position:'relative', borderRadius:'10px', left:'18%'  }}/>
         <div className="title" style={{position:'relative', borderRadius:'10px', left:'35%'}} >Login</div>
         {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
