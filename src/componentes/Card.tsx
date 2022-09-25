5b37827ad7264f8d0e9a594a9a8f1723820fa011
Backhaus <44726485+matbks@users.noreply.github.com>
2022-09-25 00:53:27 -0300

    ss

--- src/componentes/Card.tsx
+++ src/componentes/Card.tsx
@@ -168,48 +168,6 @@
 // // );
  
 
-const HoverCardDemo = () => (
-  <HoverCard>
-    <HoverCardTrigger  asChild 
-    // style={{ position: 'relative', right:70}}
-    >
-      <ImageTrigger href="https://www.instagram.com/pudimdalu01/" 
-      target="_blank" rel="noreferrer noopener" >
-        <Img src="./src/imagens/pudim.jpg" 
-        style={{height:80, width: 80 }}
-        />
-      </ImageTrigger>
-    </HoverCardTrigger>
-    <HoverCardContent sideOffset={5} >
-      <Flex css={{ flexDirection: 'column', gap: 7 }}>
-        <Img
-          size="large"
-          src="./src/imagens/pudim.jpg"
-        />
-        <Flex css={{ flexDirection: 'column', gap: 15 }}>
-          <Text>
-            <Text bold>Pudim da Lu</Text>
-            <Text faded>@pudimdalu01</Text>
-          </Text>
-          <Text>
-          O pudim número 01 🍮
-Feito com amor por @luroehrs
-📍 Entregas em Passo Fundo-RS
-Faça seu pedido 👇🏼
-wa.me/553299174320
-          </Text>
-          <Flex css={{ gap: 15 }}>
-            <Flex css={{ gap: 5 }}>
-              <Text bold>85</Text> <Text faded>Seguindo</Text>
-            </Flex>
-            <Flex css={{ gap: 5 }}>
-              <Text bold>83</Text> <Text faded>Seguidores</Text>
-            </Flex>
-          </Flex>
-        </Flex>
-      </Flex>
-    </HoverCardContent>
-  </HoverCard>
-);
+// const HoverCardDemo = (s
 
 // export default HoverCardDemo;
