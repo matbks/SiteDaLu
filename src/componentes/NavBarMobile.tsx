5f6db96a1eb946ad3a81191d959118e0cf164879
Backhaus <44726485+matbks@users.noreply.github.com>
2022-09-27 13:14:38 -0300

    ss

--- src/componentes/NavBarMobile.tsx
+++ src/componentes/NavBarMobile.tsx
@@ -10,58 +10,72 @@ function BasicExample() {
   const [width, setWidth] = React.useState(window.innerWidth);
   let wid: number = width;
   return (
-    <Navbar bg="light" expand="lg" fixed="top"  style={{borderBottomLeftRadius:'10px', 
-                                                        borderBottomRightRadius:'10px',
-                                                        position:'fixed', 
-                                                        width:'100%', 
-                                                        right:'8px' 
-                                                      }}
-                                                        >
+    <Navbar bg="light" expand="lg" fixed="top" style={{
+      borderBottomLeftRadius: '10px',
+      borderBottomRightRadius: '10px',
+      position: 'fixed',
+      width: '100%',
+      right: '8px'
+    }}
+    >
       <Container >
         <Card width={width} />
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav justify-content-center" >
-          <Nav className=" row justify-content-center">           
-          <form
-              className="container-fluid justify-content-center mt-3"
-              style={{ fontSize: 24, position: 'relative' }}
-            >
-              <button
-                className="btn btn-outline-success me-2 btn-lg"
-                type="button"
-                style={{ fontSize: 24,
-                         position: 'relative', 
-                            width: '100%' }}
-              >Login</button>
-
-            </form>
- 
+          <Nav className=" row justify-content-center">
             <form
               className="container-fluid justify-content-center mt-3"
               style={{ fontSize: 24, position: 'relative' }}
             >
-              <button
-                className="btn btn-outline-success me-2 btn-lg"
-                type="button"
-                style={{ fontSize: 24,
-                         position: 'relative', 
-                            width: '100%' }}
-              >Cadastro</button>
-
+              <a href='/cadastro'>
+                <button
+                  className="btn btn-outline-success me-2 btn-lg"
+                  type="button"
+                  style={{
+                    fontSize: 24,
+                    position: 'relative',
+                    width: '100%'
+                  }}
+                >Login</button>
+              </a>
             </form>
 
             <form
               className="container-fluid justify-content-center mt-3"
               style={{ fontSize: 24, position: 'relative' }}
             >
-              <button
-                className="btn btn-outline-success me-2 btn-lg"
-                type="button"
-                style={{ fontSize: 24,
-                         position: 'relative', 
-                            width: '100%' }}
-              >Comprar</button>
+              <a href="/cadastro">
+                <button
+                  className="btn btn-outline-success me-2 btn-lg"
+                  type="button"
 
+                  style={{
+                    fontSize: 24,
+                    position: 'relative',
+                    width: '100%'
+                  }}
+
+                >Cadastro</button>
+              </a>
+            </form>
+
+            <form
+              className="container-fluid justify-content-center mt-3"
+              style={{ fontSize: 24, position: 'relative' }}
+
+            >
+              <a href="/comprar">
+                <button
+                  className="btn btn-outline-success me-2 btn-lg"
+                  type="button"
+
+                  style={{
+                    fontSize: 24,
+                    position: 'relative',
+                    width: '100%'
+                  }}
+                >Comprar</button>
+              </a>
             </form>
 
           </Nav>
